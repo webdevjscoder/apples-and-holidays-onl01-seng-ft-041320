4cda7e24216d3832509c00c2c99037e29d361539
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 22:09:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |holiday, values|
     if holiday == 'winter'
-      values.each
+      values.each do |attribute, data|
+        
 
 end
 

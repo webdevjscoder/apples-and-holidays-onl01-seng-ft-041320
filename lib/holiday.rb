6f5317de3d737b9a8aff9d86a27894664b968fcb
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 22:47:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
     data.each do |holiday, value|
       value.each do |supply|
          if supply == "BBQ"
-            answer.push(holiday)
+            answer << holiday
           end
         end
       end

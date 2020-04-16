a47ba2decf288f498ede01887804225958dccccb
Juan Cobian <cobianjuan19@gmail.com>
2020-04-16 22:34:15 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |person, detail_hash|
-    
+    detail_hash.each do |attribute, |
   end
 end
 
